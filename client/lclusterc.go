ccdce592c8831a79b4ca91676fc3ecc91c153a8a
Robert Bisewski <contact@ibiscybernetics.com>
2017-04-28 02:15:23 -0500

    adding error codes to client

--- client/lclusterc.go
+++ client/lclusterc.go
@@ -11,6 +11,7 @@ import (
         libclient "../lib/client"
 	"flag"
 	"fmt"
+        "os"
 )
 
 // Variables to hold the result of argument flags.
@@ -24,30 +25,32 @@ var removejob int64
  */
 func main() {
 
+        // Exit return code.
+        err_code := 0
+
 	// Parse the given argument flags.
 	flag.Parse()
 
 	// If the add job flag was passed...
 	if len(addjob) > 0 {
-		addJobToServer(addjob)
-		return
-	}
+            err_code = addJobToServer(addjob)
 
 	// If the check job flag was passed...
-	if checkjob > 0 {
-		checkJobOnServer(checkjob)
-		return
-	}
+        } else if checkjob > 0 {
+	    err_code = checkJobOnServer(checkjob)
 
 	// Safety check, ensure that remove job was given a value of 1 or
 	// higher.
-	if removejob > 0 {
-		removeJobFromServer(removejob)
-		return
-	}
+        } else if removejob > 0 {
+	    err_code = removeJobFromServer(removejob)
 
 	// Otherwise just print the usage information
-	flag.Usage()
+	} else {
+	    flag.Usage()
+        }
+
+        // Send out the error code once the program is over.
+        os.Exit(err_code)
 }
 
 // Initialize the flags beforehand.
@@ -70,14 +73,14 @@ func init() {
 /*
  * @param    string    commandline to execute
  *
- * @return   none
+ * @return   int       error code
  */
-func addJobToServer(cmd string) {
+func addJobToServer(cmd string) int {
 
 	// Input validation
 	if len(cmd) < 1 {
 		fmt.Printf("addJobToServer() --> invalid input")
-		return
+		return 1
 	}
 
         // Have the client connect to the server
@@ -85,24 +88,25 @@ func addJobToServer(cmd string) {
 
 	// Safety check, ensure that no error has occurred.
 	if err != nil || response.GetPid() < 0 {
-		fmt.Printf("\nError: Unable to add job to queue!\n" +
-                  err.Error() + "\n")
-		return
+		fmt.Printf("\nError: Unable to add job to queue!\n%s\n",
+                  err.Error())
+		return 1
 	}
 
 	// Since the job was started properly, go ahead and print out a message
 	// telling the end-user the uuid of the new job.
 	fmt.Printf("The requested job has been added to queue, has pid " +
           "of %d\n", response.GetPid())
+        return 0
 }
 
 //! Status of a job from the server.
 /*
  * @param    int64    uuid of job to stat
  *
- * @return   none
+ * @return   int      error code
  */
-func checkJobOnServer(uuid int64) {
+func checkJobOnServer(uuid int64) int {
 
         // Connect to the server and check if the job is still running.
         response, err := libclient.HaveClientCheckJobOnServer(uuid)
@@ -110,35 +114,35 @@ func checkJobOnServer(uuid int64) {
 	// Safety check, ensure that no error has occurred.
 	if err != nil {
             fmt.Printf(err.Error())
-            return
+            return 1
 	}
 
 	// If an internal server-side error has occurred...
 	if response.Rc == lcfg.CjrCorruptedServerInput {
 		fmt.Printf("An internal server-side error has occurred.\n")
-		return
+		return 1
 
 		// Unknown job state
 	} else if response.Rc == lcfg.CjrUnknown {
 		fmt.Printf("The job %d appears to have an unknown " +
 			   "status.\nConsider contacting the server " +
                            "operator.\n", uuid)
-		return
+		return 0
 
 		// Job does not exist
 	} else if response.Rc == lcfg.CjrProcessNotExist {
 		fmt.Printf("The job %d is not present.\n", uuid)
-		return
+		return 0
 
 		// Job is queued
 	} else if response.Rc == lcfg.CjrProcessQueued {
 		fmt.Printf("The job %d is queued.\n", uuid)
-		return
+		return 0
 
 		// Job is currently running
 	} else if response.Rc == lcfg.CjrProcessActive {
 		fmt.Printf("The job %d is currently active.\n", uuid)
-		return
+		return 0
 	}
 
 	// otherwise if none of the above happened, print out a message
@@ -146,6 +150,7 @@ func checkJobOnServer(uuid int64) {
 	fmt.Printf("The following undefined response code was " +
           "given back: %d\nConsider contacting the server operator.\n",
           response.Rc)
+        return 1
 }
 
 //! Remove a job from the server.
@@ -154,12 +159,12 @@ func checkJobOnServer(uuid int64) {
  *
  * @return   none
  */
-func removeJobFromServer(uuid int64) {
+func removeJobFromServer(uuid int64) int {
 
 	// Input validation
 	if uuid < 1 {
 		fmt.Printf("removeJobFromServer() --> invalid input")
-		return
+		return 1
 	}
 
         // Tell the server the Uuid of the job to stop.
@@ -168,9 +173,9 @@ func removeJobFromServer(uuid int64) {
 	// check the return code from the StopJobResponse object, if it -1 then
 	// an error has occurred.
 	if err != nil || response.Rc == lcfg.SjrFailure {
-		fmt.Printf("Server has responded with the following error:\n")
-		fmt.Printf(response.Error + "\n")
-		return
+		fmt.Printf("Server has responded with the following error: \n%s\n",
+                  response.Error)
+		return 1
 	}
 
 	// with a return code of `1` then the process could not be found, but
@@ -178,13 +183,13 @@ func removeJobFromServer(uuid int64) {
 	// request
 	if response.Rc == lcfg.SjrDoesNotExist {
                 fmt.Printf("No such process exists with given uuid: %d\n", uuid)
-		return
+		return 0
 	}
 
 	// Since this has obtained a response, go ahead and return the result
 	if response.Rc == lcfg.SjrSuccess {
 		fmt.Printf("Successfully removed job %d\n", uuid)
-		return
+		return 0
 	}
 
 	// otherwise if none of the above happened, print out a message
@@ -192,4 +197,5 @@ func removeJobFromServer(uuid int64) {
 	fmt.Printf("The following undefined response code was given " +
           "back: %d\nConsider contacting the server operator.\n",
           response.Rc)
+        return 0
 }
